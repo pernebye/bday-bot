219c56852ad2426abbc140f641a0058081ef731c
pernebye <pernebye@gmail.com>
2024-12-10 00:33:17 +0500

    Первый коммит для Telegram-бота

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fetch = require('node-fetch');
+
+const app = express();
+app.use(bodyParser.json());
+
+const BOT_TOKEN = '7565594920:AAFr_PBq22jFw5Fxa3YMl2jlyI5IaLpNTEo';
+const CHAT_ID = '1026864669';
+
+app.post('/notify', (req, res) => {
+    const { name } = req.body;
+
+    fetch(`https://api.telegram.org/bot${BOT_TOKEN}/sendMessage`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            chat_id: CHAT_ID,
+            text: `Новый гость: ${name} подтвердил "Приду!"`,
+        }),
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            console.log('Сообщение отправлено:', data);
+            res.status(200).send('Уведомление отправлено');
+        })
+        .catch((error) => {
+            console.error('Ошибка при отправке сообщения:', error);
+            res.status(500).send('Ошибка при отправке уведомления');
+        });
+});
+
+app.listen(3000, () => console.log('Сервер запущен на http://localhost:3000'));
